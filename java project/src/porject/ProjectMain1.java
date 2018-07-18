9cc3bd3349cad15929434d95e296e1fdcf106c5c
jacob940910 <dltmdghks94@gmail.com>
2018-07-18 19:11:36 +0900

    java study subs 0718

--- java project/src/porject/ProjectMain1.java
+++ java project/src/porject/ProjectMain1.java
@@ -9,7 +9,7 @@ public class ProjectMain1 {
 			for(j=0;j<5-i;j=j+1) {
 				System.out.print(" ");
 			}
-			//10보다작을떄 %
+			//10보다작을때 %
 			System.out.print(idx%10 );
 			idx=idx+1;
 			if(i>=1 && i<=4) {
@@ -19,13 +19,14 @@ public class ProjectMain1 {
 				System.out.print(idx%10);
 				idx=idx+1;
 			}
+			//맨밑의 0부터9까지 
 			if(i==5) {
 				for(j=0;j<2*i;j=j+1) {
 				System.out.print(idx%10);
 				idx=idx+1;
 				}
 			}
-			
+			//for문이끝날때마다줄바꿈을 위해서 println을이용
 			System.out.println(" ");
 		}
 		
---
+++ javastudysubs/src/javastudysubs0718/Main1.java
@@ -0,0 +1,20 @@
+package javastudysubs0718;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		int x1 = 10;
+		int x2 = 20;
+		double d1= 10.3;
+		
+		double d2 = x1 / x2;
+		System.out.println("d2:"+d2);
+		
+		d2=(double)x1/x2;
+		System.out.println("d2:"+d2);
+		
+		System.out.println(10%3);
+
+	}
+
+}
