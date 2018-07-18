6bd68399f0326bad310547283a3dbcc9976ef53f
jacob940910 <dltmdghks94@gmail.com>
2018-07-18 19:10:39 +0900

    java study project

---
+++ java project/src/porject/ProjectMain1.java
@@ -0,0 +1,35 @@
+package porject;
+
+public class ProjectMain1 {
+
+	public static void main(String[] args) {
+		int idx=0;
+		for(int i=0;i<6;i=i+1) {
+			int j;
+			for(j=0;j<5-i;j=j+1) {
+				System.out.print(" ");
+			}
+			//10보다작을떄 %
+			System.out.print(idx%10 );
+			idx=idx+1;
+			if(i>=1 && i<=4) {
+				for(j=0;j<2*i-1;j=j+1) {
+					System.out.print(" ");
+				}
+				System.out.print(idx%10);
+				idx=idx+1;
+			}
+			if(i==5) {
+				for(j=0;j<2*i;j=j+1) {
+				System.out.print(idx%10);
+				idx=idx+1;
+				}
+			}
+			
+			System.out.println(" ");
+		}
+		
+		
+	}
+
+}
