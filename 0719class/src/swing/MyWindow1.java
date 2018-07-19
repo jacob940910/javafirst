17c7466087eaa3701c9d08e9b0233c33d97b7808
jacob940910 <dltmdghks94@gmail.com>
2018-07-19 15:20:13 +0900

    0719 javastudy

---
+++ 0719class/src/swing/MyWindow1.java
@@ -0,0 +1,26 @@
+package swing;
+
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+
+public class MyWindow1 extends JFrame {
+   String[] data = { "Encapsulation", "Inheritance", "Polymorphism", "Information Hiding" };
+
+   // 윈도우 클래스
+   public MyWindow1() {
+      setBounds(500, 500, 1000, 1000);
+      setTitle("스윙");
+      setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+      JList<String>list = new JList<>(data);
+      JComboBox<String>combo = new JComboBox<>(data);
+      JPanel p = new JPanel();
+      p.add(list);
+      p.add(combo);
+      add(p);
+      
+      setVisible(true);
+   }
+}
